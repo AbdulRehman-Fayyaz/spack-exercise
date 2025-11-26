081ffb2daaa88d31e4b1d9743cdab060e2de386a
Abdul Rehman <abdulrehmanfayyaz123@gmail.com>
2025-11-26 17:17:14 +0100

    Add optional dependencies for Boost and YAML

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
-#include "flatset/flatset.hpp"
-#include "filesystem/filesystem.hpp"
-#include "yamlParser/yamlParser.hpp"
 #include <iostream>
+
+#ifdef USE_BOOST
+  #include "flatset/flatset.hpp"
+  #include "filesystem/filesystem.hpp"
+#endif
+
+#ifdef USE_YAML
+  #include "yamlParser/yamlParser.hpp"
+#endif
 
 int main(int argc, char *argv[])
 {
   std::cout << "Let's fight with CMake, Docker, and some dependencies!" << std::endl << std::endl;
 
+#ifdef USE_BOOST
   std::cout << "Modify a flat set using boost container" << std::endl;
   modifyAndPrintSets();
   std::cout << std::endl;
@@ -14,7 +21,11 @@ int main(int argc, char *argv[])
   std::cout << "Inspect the current directory using boost filesystem" << std::endl;
   inspectDirectory();
   std::cout << std::endl;
+#else
+  std::cout << "Boost is disabled." << std::endl;
+#endif
 
+#ifdef USE_YAML
   if ( argc == 2 )
   {
     const std::string yamlFile( argv[1] );
@@ -22,7 +33,9 @@ int main(int argc, char *argv[])
     std::cout << "  " << yamlFile << std::endl;
     parseConfig( yamlFile );
   }
+#else
+  std::cout << "YAML-CPP is disabled." << std::endl;
+#endif
 
   return 0;
 }
-
